5bde8780c31d5057bc69f7fd34f675216baa2738
Xavier Martinez <blackshieldsx@gmail.com>
2020-12-05 14:23:31 -0800

    # This will print Hello 10 times with a corresponding # but go ahead 2
    # This will print Hello 10 times with a corresponding number but go ahead 2 every time
    for x in range(0, 10, 2):
        # print(i) will add a number next to each 'Salam"
        print(x)
        print('Salam')

--- main.py
+++ main.py
@@ -4,13 +4,27 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-for i in range(0, 10):
-    print('Hello')
 # This will print Hello 10 times
+for i in range(0, 10):
+    # print(i) will add a number next to each 'Hello"
+    print(i)
+    print('Hello')
 
+# This will print Hello 10 times with a corresponding number but go ahead 2 every time
+for x in range(0, 10, 2):
+    # print(i) will add a number next to each 'Salam"
+    print(x)
+    print('Salam')
+
+# This will print a count to 10 and that will be: 0,1,2,3,4,5,6,7,8,9
 for n in range(0, 10):
     print(n)
-# This will print a count to 10 and that will be: 0,1,2,3,4,5,6,7,8,9
+
+# This will print all numbers in the list in the order I put it
+my_list = [2, 6, 21, 8, 12, 15, 18]
+
+for a in my_list:
+    print(a)
 
 # Press the green button in the gutter to run the script.
 
