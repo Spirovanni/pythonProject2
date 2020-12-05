0b8e1a71e5e411b82badc58f451dea4829616f3e
Xavier Martinez <blackshieldsx@gmail.com>
2020-12-05 12:13:08 -0800

    name = input('> ')

--- main.py
+++ main.py
@@ -4,6 +4,17 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+print('Please enter your name ?')
+
+# name = input('> ') will ask the user for their name
+name = input('> ')
+
+
+print('Hi ' + name)
+print('Now please enter your family name')
+
+family_name = input('> ')
+print('OK ' + family_name)
 
 # Press the green button in the gutter to run the script.
 
