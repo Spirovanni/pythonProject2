682d23863468a1aab6b48c3b8c31dbe393919319
Xavier Martinez <blackshieldsx@gmail.com>
2020-12-05 13:47:16 -0800

    # This will print Hello 10 times

--- main.py
+++ main.py
@@ -4,19 +4,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-print('Enter your age ')
-
-age = int(input())
-
-if age < 18:
-    print('you cannot enter ')
-elif 18 < age < 60:
-    print('you can enter')
-elif age > 60:
-    print('your too old to enter!')
-else:
-    print('Please enter your age')
-
+for i in range(0, 10):
+    print('Hello')
+# This will print Hello 10 times
 
 # Press the green button in the gutter to run the script.
 
