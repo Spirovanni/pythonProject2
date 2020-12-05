efba8f1cbe21859dc05ef55083b9fc3f1e9269bd
Xavier Martinez <blackshieldsx@gmail.com>
2020-12-05 10:08:58 -0800

    list_1[0]

--- main.py
+++ main.py
@@ -2,7 +2,15 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+list_1 = ['Mike', 'Alan', 'John', 'Cary']
 
+list_2 = ['Barry', 'Chuck', 'Rick Ross', 'Larry']
+
+print(list_1[0])
+
+a = list_1[1]
+b = list_1[2]
+c = list_1[3]
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
