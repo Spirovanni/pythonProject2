97838dd6e4b15747bb8840d410e31ccc4a155e50
Xavier Martinez <blackshieldsx@gmail.com>
2020-12-05 11:12:53 -0800

    list_2.insert(2, 'terri') will insert 'terri' in the 2 spot of list_2

--- main.py
+++ main.py
@@ -10,24 +10,27 @@ a = list_1[0]
 b = list_1[2]
 c = list_1[3]
 d = list_2[2]
-
 a = a.title()
 b = b.title()
 c = c.title()
 d = d.title()
+# list_1.append('roger') adds 'roger' to list_1
+list_1.append('roger')
+# list_2.insert(2, 'terri') will insert 'terri' in the 2 spot of list_2
+list_2.insert(2, 'terri')
+h = list_2[2]
+h = h.title()
 # e = (len(list_1)) creates a count of List_1
 e = (len(list_1))
 f = (len(list_2))
 g = e + f
-# list_1.append('roger') adds 'roger' to list_1
-list_1.append('roger')
 
 
 def print_hi(name):
     # name = name.title() will capitalize the first letters in each word
     name = name.title()
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Welcome, {name}, {a}, {d} and all {g} of your friends!')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Welcome, {name}, {a}, {d}, {h} and all {g} of your friends!')  # Press Ctrl+F8 to toggle the breakpoint.
     print(list_1)
     print(list_2)
 
