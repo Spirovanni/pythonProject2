077c9d2117191bff07036b1f3a980c6919e541fa
Xavier Martinez <blackshieldsx@gmail.com>
2020-12-05 20:01:03 -0800

    # This will create a database that can be inputted into and will return data based off of input
    database2 = [
        ['ali', 'robert', 'ritchie', 'terri'],
        ['smith', 'rogers', 'bangino', 'gomez'],
        [23, 22, 21, 45]
    ]
    first_name = database2[0]
    family_name = database2[1]
    ages = database2[2]
    
    input_1 = str(input('Please enter user or pass: '))
    if input_1 == 'f' or input_1 == 'firstname':
        print(first_name)
    elif input_1 == 'l' or input_1 == 'lastname':
        print(family_name)
    elif input_1 == 'a' or input_1 == 'ages':
        print(ages)
    else:
        print('Please enter the correct info!')

--- main.py
+++ main.py
@@ -25,4 +25,24 @@ database = [
 
 print(database)
 
+# This will create a database that can be inputted into and will return data based off of input
+database2 = [
+    ['ali', 'robert', 'ritchie', 'terri'],
+    ['smith', 'rogers', 'bangino', 'gomez'],
+    [23, 22, 21, 45]
+]
+first_name = database2[0]
+family_name = database2[1]
+ages = database2[2]
+
+input_1 = str(input('Please enter user or pass: '))
+if input_1 == 'f' or input_1 == 'firstname':
+    print(first_name)
+elif input_1 == 'l' or input_1 == 'lastname':
+    print(family_name)
+elif input_1 == 'a' or input_1 == 'ages':
+    print(ages)
+else:
+    print('Please enter the correct info!')
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
