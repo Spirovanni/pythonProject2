21f411dca17b17901663f282303084e1d54655a8
Xavier Martinez <blackshieldsx@gmail.com>
2020-12-05 20:09:31 -0800

    19. List use if and else with input with list
    # If this list has anything in it then print it
    l = []
    
    if l:
        for i in l:
            print(l)
    else:
        print('There is no items in l')

--- main.py
+++ main.py
@@ -1,48 +1,19 @@
 # Section 7: List, Conditional, Loop, Input
-# 17. List and itrat in details
+# 19. List use if and else with input with list
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 # Press the green button in the gutter to run the script.
 
-# This will use the n list created and split it up by the space
-n = 'ali mitch robert rick john dan'
-name = n.split()
-print(name)
 
-# This will use the n list created and split it up by the dot
-n = 'ali.mitch.robert.rick.john.dan.mike.andrea.renee.ass'
-name = n.split('.')
-print(name)
+# If this list has anything in it then print it
+l = []
 
-# This will create a database
-database = [
-    ['ali', 'robert', 'ritchie', 'terri'],
-    ['smith', 'rogers', 'bangino', 'gomez'],
-    [23, 22, 21, 45]
-]
-
-print(database)
-
-# This will create a database that can be inputted into and will return data based off of input
-database2 = [
-    ['ali', 'robert', 'ritchie', 'terri'],
-    ['smith', 'rogers', 'bangino', 'gomez'],
-    [23, 22, 21, 45]
-]
-first_name = database2[0]
-family_name = database2[1]
-ages = database2[2]
-
-input_1 = str(input('Please enter user or pass: '))
-if input_1 == 'f' or input_1 == 'firstname':
-    print(first_name)
-elif input_1 == 'l' or input_1 == 'lastname':
-    print(family_name)
-elif input_1 == 'a' or input_1 == 'ages':
-    print(ages)
+if l:
+    for i in l:
+        print(l)
 else:
-    print('Please enter the correct info!')
+    print('There is no items in l')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
